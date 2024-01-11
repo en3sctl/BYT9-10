74d603cd29687c8a223aee126a4b33df48fd098d
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2024-01-11 21:56:36 +0100

    Update Investigator.java

--- src/main/java/Investigator.java
+++ src/main/java/Investigator.java
@@ -13,38 +13,4 @@ public class Investigator extends Player
 
     public void revealRole(int id) {}
 
-    @Override
-    public void login() {
-
-    }
-
-    @Override
-    public void createRoom() {
-
-    }
-
-    @Override
-    public void vote() {
-
-    }
-
-    @Override
-    public void joinRoom() {
-
-    }
-
-    @Override
-    public void leaveRoom() {
-
-    }
-
-    @Override
-    public void enableRole() {
-
-    }
-
-    @Override
-    public void disableRole() {
-
-    }
 }
